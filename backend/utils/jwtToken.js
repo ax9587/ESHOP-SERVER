77b70c1187dad668fba1fc158e36ec2dcb091180
ax9587 <ax9587@gmail.com>
2023-05-20 22:29:04 -0600

    samesite

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,7 +7,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     Secure: true,
-    SameSite: 'none'
+    sameSite: false
   };
 
   res.status(statusCode).cookie("user_token", token, options).json({
